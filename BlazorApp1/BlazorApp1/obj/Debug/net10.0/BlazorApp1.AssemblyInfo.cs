647a5bf0b480f076b8eaf30a5caea6cace412acb
khrispatel2894-web <khrispatel2894@yahoo.com>
2026-04-15 12:49:11 -0500

    fixes
    fixed the timestamp on the posts not being from this time zone

--- BlazorApp1/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4180b57932227ab143d3af1f86469c4e3d03463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da79977751293a60ccab17bcc553a638bc77046d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
